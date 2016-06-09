b5d916a2efaefdb415eee925ab6896815e956950
Fabian Pombo <contact@fabianpombo.com>
2016-06-09 17:32:35 -0400

    completed lesson4/ex1

---
+++ fourth_lesson/exercise_1/quiz.js
@@ -0,0 +1,7 @@
+var answer = prompt('What programming langugae is the name of a gem?');
+
+if (answer.toUpperCase() === 'RUBY') {
+  document.write('<p>That is totally right. Ruby rocks! Get it?</p>');
+} else {
+  document.write('<h2>Wrong!</h2>');
+}
