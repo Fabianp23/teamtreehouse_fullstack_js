4c8dc2e003b925bbabbd1bf444a2e9abb409581b
Fabian Pombo <contact@fabianpombo.com>
2016-06-09 17:14:58 -0400

    completed lesson3/code challange

---
+++ third_lesson/code_challenge/random.js
@@ -0,0 +1,4 @@
+var userNumber = prompt('Input a random number, porfavor');
+var randomNumber = Math.floor(Math.random() * parseInt(userNumber)) + 1;
+
+alert('Your random number is: ' + randomNumber);
