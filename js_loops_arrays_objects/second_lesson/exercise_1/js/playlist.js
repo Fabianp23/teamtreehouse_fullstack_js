a61d489e640d1062eefb29ec763743188ef5a972
Fabian Pombo <contact@fabianpombo.com>
2016-06-14 14:32:41 -0400

    completed chapter2/lesson2/ex1

---
+++ js_loops_arrays_objects/second_lesson/exercise_1/js/playlist.js
@@ -0,0 +1,7 @@
+var playList = [];
+playList.push('I did it my way');
+playList.push('Respect', 'Imagine');
+playList.unshift('Born to Run');
+playList.unshift('Louie Louie', 'Maybellene');
+
+printList(playList);
