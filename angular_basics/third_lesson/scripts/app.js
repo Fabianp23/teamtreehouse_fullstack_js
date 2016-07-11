811bde6b853991e79d9a9c56e5cac333374b0437
Fabian Pombo <contact@fabianpombo.com>
2016-07-11 18:56:21 -0400

    reviewed code. removed unnecessary function in 'mainCtrl'

--- angular_basics/third_lesson/scripts/app.js
+++ angular_basics/third_lesson/scripts/app.js
@@ -1,17 +1,13 @@
 angular.module('todoListApp', [])
 
-.controller('mainCtrl', function($scope){
-
-  $scope.learningNgChange = function(){
-    console.log('An input changed');
-  };
+.controller('mainCtrl', function($scope) {
 
   $scope.todos = [
-    {'name': 'clean the house'},
-    {'name': 'water the dog'},
-    {'name': 'feed the lawn'},
-    {'name': 'pay dem bills'},
-    {'name': 'swim'},
+    {"name": "clean the house"},
+    {"name": "wash the self"},
+    {"name": "wash blackylin"},
+    {"name": "wash silverin"},
+    {"name": "wash kikolin"}
   ]
 
 });
